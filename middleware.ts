d213344e0274f95e3032a6e37837000d048111fd
Vansh singh sura <vanshsura29@gmail.com>
2025-01-30 15:52:50 +0530

    fix some issues

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,7 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 import { NextResponse } from "next/server";
 
-const isProtectedRoute = createRouteMatcher([
-  "/dashboard(.*)",
-  "/forum(.*)",
-  "/api/webhooks(.*)",
-]);
+const isProtectedRoute = createRouteMatcher(["/dashboard(.*)", "/forum(.*)"]);
 export default clerkMiddleware(async (auth, req) => {
   if (isProtectedRoute(req)) await auth.protect();
 });
@@ -13,9 +9,9 @@ export default clerkMiddleware(async (auth, req) => {
 export function middleware() {
   const res = NextResponse.next();
 
-  // add the CORS headers to the response
+
   res.headers.append("Access-Control-Allow-Credentials", "true");
-  res.headers.append("Access-Control-Allow-Origin", "http://localhost:3000"); // replace this your actual origin
+  res.headers.append("Access-Control-Allow-Origin", "http://localhost:3000"); 
   res.headers.append(
     "Access-Control-Allow-Methods",
     "GET,DELETE,PATCH,POST,PUT"
